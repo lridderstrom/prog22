adacfa0168affae3de7bd456b76111263b6b28f0
lridderstrom <linnea.ridderstrom.5181@student.uu.se>
2023-10-11 11:34:58 +0200

    change again

--- person.cpp
+++ person.cpp
@@ -25,11 +25,11 @@ void Person::set(int n){
 	}
 
 int Person::fib(){
-	return fib_(age);
-int Person::fibi(int n){
-	if (n <= 1)
-		return n;
-	return fibi(n-1) + fibi(n-2);
+	return fibi(age);
+	int Person::fibi(int n){
+		if (n <= 1)
+			return n;
+		return fibi(n-1) + fibi(n-2);
 	}	
 }
 
@@ -38,7 +38,7 @@ extern "C"{
 	Person* Person_new(int n) {return new Person(n);}
 	int Person_get(Person* person) {return person->get();}
 	void Person_set(Person* person, int n) {person->set(n);}
-	int Person_fib(Person * person, int n) {return person -> fib(n);}
+	int Person_fib(Person * person, int n) {return person -> fib();}
 	void Person_delete(Person* person){
 		if (person){
 			delete person;
